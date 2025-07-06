6fd0a07a8f1593bd28d248954f41654fee78a624
mahakamal-e <mahakamal09@gmail.com>
2025-07-06 15:41:38 +0300

    add EXPLAIN ANALYZE to measure performance  before and aft er adding index

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,13 @@
+-- Measure performance before creating indexes
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = 'sample-user-id';
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE property_id = 'sample-property-id';
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE start_date BETWEEN '2024-01-01' AND '2024-12-31';
+
 -- Existing useful index
 CREATE INDEX idx_user_email ON User (email);
 
@@ -6,3 +16,14 @@ CREATE INDEX idx_booking_user_id ON Booking (user_id);
 CREATE INDEX idx_booking_property_id ON Booking (property_id);
 CREATE INDEX idx_booking_start_date ON Booking (start_date);
 CREATE INDEX idx_property_host_id ON Property (host_id);
+
+-- Measure performance after creating indexes
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = 'sample-user-id';
+
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE property_id = 'sample-property-id';
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE start_date BETWEEN '2024-01-01' AND '2024-12-31';

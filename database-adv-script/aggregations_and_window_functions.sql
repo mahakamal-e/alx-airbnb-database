5f89b0ce5f3b61effb76331c54870542aec78e88
mahakamal-e <mahakamal09@gmail.com>
2025-07-06 15:18:37 +0300

    edit 2

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,28 +1,29 @@
 -- Query 1: Total number of bookings made by each user
 SELECT 
-    u.user_id, 
-    u.first_name, 
-    u.last_name, 
+    u.user_id,
+    u.first_name,
+    u.last_name,
     COUNT(b.booking_id) AS total_bookings
 FROM 
     User u
 LEFT JOIN 
     Booking b ON u.user_id = b.user_id
 GROUP BY 
-    u.user_id, u.first_name, u.last_name;
+    u.user_id, u.first_name, u.last_name
+ORDER BY 
+    total_bookings DESC;
 
 -- Query 2: Rank properties based on total number of bookings using RANK()
 SELECT 
-    p.property_id, 
-    p.name, 
-    p.location, 
+    p.property_id,
+    p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
 FROM 
     Property p
 LEFT JOIN 
     Booking b ON p.property_id = b.property_id
 GROUP BY 
-    p.property_id, p.name, p.location
+    p.property_id, p.name
 ORDER BY 
-    rank;
+    property_rank;
